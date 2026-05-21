94d6713fbf09de393d66cc23b5763c1c10672e49
Rahul Gavit <rahul@easyhrworld.com>
2026-05-21 16:19:44 +0530

    changes in .env

--- config/db.js
+++ config/db.js
@@ -3,17 +3,18 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 connection.connect((err) => {
   if (err) {
     console.log("Database connection failed:", err);
   } else {
-    console.log("MySQL Connected");
+    console.log("MySQL Connected Successfully");
   }
 });
 
